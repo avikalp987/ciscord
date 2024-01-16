de82cdecabb7f531c53931589119cc3d06320776
vikalp <avikalp233@gmail.com>
2024-01-16 16:38:16 +0530

    Dark and light theme setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs"
+import { ThemeProvider } from '@/components/provider/theme-provider'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -17,8 +19,19 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={font.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(font.className,
+        "bg-light dark:bg-[#313338]"
+        )}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem={false}
+          storageKey='ciscord-theme'
+        >
+        {children}
+        </ThemeProvider>
+      </body>
     </html>
     </ClerkProvider>
   )
