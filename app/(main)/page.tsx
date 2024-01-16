a629b769a41d3df92340259f3e3e84815d233ccf
vikalp <avikalp233@gmail.com>
2024-01-16 08:41:36 +0530

    Authentication

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      This is a protected route.
+      <UserButton 
+        afterSignOutUrl="/"
+      />
     </div>
   )
 }
