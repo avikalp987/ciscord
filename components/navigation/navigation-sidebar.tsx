ada2478b183772a322296bb34663c79b1c97331b
vikalp <avikalp233@gmail.com>
2024-01-18 00:10:02 +0530

    Navigation sidebar

---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,72 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import NavigationAction from "./navigation-action";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import NavigationItem from "./navigation-item";
+import { ModeToggle } from "../mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+const NavigationSidebar = async () => {
+
+    //fetch the current profile
+    const profile = await currentProfile()
+
+    if(!profile)
+    {
+        return redirect("/")
+    }
+
+    //find all the servers which this user is a part of
+    const servers = await db.server.findMany({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+
+    return ( 
+        <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3">
+            <NavigationAction />
+
+            <Separator 
+                className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+            />
+
+            <ScrollArea
+                className="flex-1 w-full"
+            >
+                {servers.map((server) => (
+                    <div
+                        className="mb-4"
+                        key={server.id}
+                    >
+                        <NavigationItem 
+                            id={server.id}
+                            imageUrl={server.imageUrl}
+                            name={server.name}
+                        />
+                    </div>
+                ))}
+            </ScrollArea>
+
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle />
+                <UserButton 
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                            avatarBox: "h-[48px] w-[48px]"
+                        }
+                    }}
+                />
+            </div>
+        </div>
+     );
+}
+ 
+export default NavigationSidebar;
