48022b471beadde2cf938e439e7950af46775332
vikalp <avikalp233@gmail.com>
2024-01-22 19:05:32 +0530

    Chat Header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,6 +1,55 @@
-const ChannelIdPage = () => {
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+    params: {
+        serverId: string,
+        channelId: string,
+    }
+}
+
+const ChannelIdPage = async ({
+    params
+}: ChannelIdPageProps) => {
+
+    const profile = await currentProfile()
+
+    if(!profile)
+    {
+        return redirectToSignIn()
+    }
+
+    //fetch our channel
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId
+        }
+    })
+
+    //fetch our member
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id
+        }
+    })
+
+    if(!channel || !member)
+    {
+        redirect("/")
+    }
+
     return ( 
-        <div>channel Id page</div>
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
+        </div>
      );
 }
  
---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,35 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
+import NavigationSidebar from "./navigation/navigation-sidebar";
+import ServerSidebar from "./server/server-sidebar";
+
+const MobileToggle = ({
+    serverId
+}: {
+    serverId: string
+}) => {
+    return ( 
+        <div>
+            <Sheet>
+
+                <SheetTrigger asChild>
+                    <Button variant={"ghost"} size={"icon"} className="md:hidden">
+                        <Menu />
+                    </Button>
+                </SheetTrigger>
+
+
+                <SheetContent side={"left"} className="p-0 flex gap-0">
+                    <div className="w-[72px]">
+                        <NavigationSidebar />
+                    </div>
+
+                    <ServerSidebar serverId={serverId}/>
+                </SheetContent>
+            </Sheet>
+        </div>
+     );
+}
+ 
+export default MobileToggle;
