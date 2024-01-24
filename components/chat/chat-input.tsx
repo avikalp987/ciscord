6527fc4f34ae66c62c46a816da31d7ce05dab062
vikalp <avikalp233@gmail.com>
2024-01-24 22:52:20 +0530

    Message Attachment

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -8,6 +8,7 @@ import { Plus, Smile } from "lucide-react"
 import { Input } from "../ui/input"
 import qs from "query-string"
 import axios from "axios"
+import { useModal } from "@/hooks/use-modal-store"
 
 interface ChatInputProps{
     apiUrl: string,
@@ -28,6 +29,8 @@ const ChatInput = ({
     name,
     type,
 }: ChatInputProps) => {
+
+    const { onOpen } = useModal()
 
     //creating our form hook 
     const form = useForm<z.infer<typeof formSchema>>({
@@ -68,7 +71,7 @@ const ChatInput = ({
                                 <div className="relative p-4 pb-6">
                                     <button
                                         type="button"
-                                        onClick={() => {}}
+                                        onClick={() => onOpen("messageFile", { apiUrl, query })}
                                         className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                                     >
                                         <Plus className="text-white dark:text-[#313338]"/>
--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,7 +2,7 @@
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css"
 import { error } from "console";
-import { X } from "lucide-react";
+import { File, FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 interface FileUploadProps{
@@ -36,6 +36,33 @@ const FileUpload = (
                 <button
                     onClick={()=>onChange("")}
                     className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+                    type="button"
+                >
+                    <X 
+                        className="h-4 w-4"
+                    />
+                </button>
+            </div>
+        )
+    }
+
+    if(value && fileType==="pdf")
+    {
+        return (
+            <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+                <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400"/>
+                <a 
+                href={value} 
+                target="_blank" 
+                rel="noopener norefferer"
+                className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+                >
+                    {value}
+                </a>
+
+                <button
+                    onClick={()=>onChange("")}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
                     type="button"
                 >
                     <X 
