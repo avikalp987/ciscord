48980366cae5e95928b146d1b4cb12f4b38d30c4
vikalp <avikalp233@gmail.com>
2024-01-26 23:19:50 +0530

    Chat socket hook

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -16,6 +16,7 @@ import { Input } from "../ui/input"
 import { Button } from "../ui/button"
 import axios from "axios"
 import { useModal } from "@/hooks/use-modal-store"
+import { useParams, useRouter } from "next/navigation"
 
 interface ChatItemProps{
     id: string,
@@ -58,6 +59,20 @@ export const ChatItem = ({
     const [isEditing, setIsEditing] = useState(false)
 
     const { onOpen } = useModal()
+
+    const params = useParams()
+    const router = useRouter()
+
+
+    const onMemberclick = () => {
+        if(member.id === currentMember.id)
+        {
+            return;
+        }
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+
+    }
+
 
     //press escape to cancel
     useEffect(() => {
@@ -124,7 +139,7 @@ export const ChatItem = ({
 
     return <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
         <div className="group flex gap-x-2 items-start w-full">
-            <div className="cursor-pointer hover:drop-shadow-md transition">
+            <div onClick={onMemberclick} className="cursor-pointer hover:drop-shadow-md transition">
                 <UserAvatar 
                     src={member.profile.imageUrl}
                 />
@@ -133,7 +148,7 @@ export const ChatItem = ({
             <div className="flex flex-col w-full">
                 <div className="flex items-center gap-x-2">
                     <div className="flex items-center">
-                        <p className="font-semibold text-sm hover:underline cursor-pointer">{member.profile.name}</p>
+                        <p onClick={onMemberclick} className="font-semibold text-sm hover:underline cursor-pointer">{member.profile.name}</p>
 
                         <ActionTooltip label={member.role}>
                             {roleIconMap[member.role]}
--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import { format } from "date-fns"
 import { ChatItem } from "./chat-item";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
@@ -41,6 +42,8 @@ const ChatMessages = ({
 }: ChatMessagesProps) => {
 
     const queryKey = `chat:${chatId}`
+    const addKey = `chat:${chatId}:messages`
+    const updateKey = `chat:${chatId}:messages:update`
 
     const {
         data,
@@ -54,6 +57,10 @@ const ChatMessages = ({
         paramKey,
         paramValue
     })
+
+
+    useChatSocket({ queryKey, addKey, updateKey })
+
 
     if(status === "loading")
     {
