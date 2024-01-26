d78e28682bc8dc3a3fa495f86876f022843e6c12
vikalp <avikalp233@gmail.com>
2024-01-26 11:42:29 +0530

    Chat messages component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -44,14 +45,29 @@ const ChannelIdPage = async ({
     }
 
     return ( 
-        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full flex-1">
             <ChatHeader
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
             />
 
-            <div className="flex-1">Future Messages</div>
+            <div className="flex-1">
+                <ChatMessages 
+                    member={member}
+                    name={channel.name}
+                    chatId={channel.id}
+                    type="channel"
+                    apiUrl="/api/messages"
+                    socketUrl="/api/socket/messages"
+                    socketQuery={{
+                        channelId: channel.id,
+                        serverId: channel.serverId,
+                    }}
+                    paramKey="channelId"
+                    paramValue={channel.id}
+                />
+            </div>
             <ChatInput
                 name={channel.name}
                 type="channel"
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/provider/theme-provider'
 import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/provider/modal-provider'
 import { SocketProvider } from '@/components/provider/socket-provider'
+import { QueryProvider } from '@/components/provider/query-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -33,7 +34,9 @@ export default function RootLayout({
         >
           <SocketProvider>
             <ModalProvider />
+            <QueryProvider>
             {children}
+            </QueryProvider>
           </SocketProvider>
         </ThemeProvider>
       </body>
